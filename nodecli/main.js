2454ecbf8acfd0f75b8117ad14b18146ad67c5ec
yota-yamazaki <yota.yamazaki@rightcode.co.jp>
2025-11-13 12:00:54 +0900

    nodecli

---
+++ nodecli/main.js
@@ -0,0 +1,30 @@
+import * as util from "node:util";
+import * as fs from "node:fs/promises";
+import { md2html } from "./md2html.js";
+
+// コマンドライン引数からファイルパスとオプション/フラグを受け取る
+const {
+    values,
+    positionals
+} = util.parseArgs({
+    allowPositionals: true,
+    options: {
+        // gfmフラグを定義する
+        gfm: {
+            type: "boolean",
+            default: false,
+        }
+    }
+});
+const filePath = positionals[0];
+fs.readFile(filePath, { encoding: "utf8" }).then(file => {
+    // md2htmlモジュールを使ってHTMLに変換する
+    const html = md2html(file, {
+        // gfmフラグのパース結果をオプションとして渡す
+        gfm: values.gfm
+    });
+    console.log(html);
+}).catch(err => {
+    console.error(err.message);
+    process.exit(1);
+});
